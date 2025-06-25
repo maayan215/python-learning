60a2bc608190fa6ac180560f01c3d0a99353749f
maayan <maayan.aharonn@gmail.com>
2025-06-25 23:51:54 +0300

    Calculating Linear Regression

--- task1_2.py
+++ task1_2.py
@@ -1,8 +1,6 @@
-import numpy as np
-from scipy import stats
-import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 points = np.array([
   [2, 4],
   [3, 4],
@@ -14,7 +12,13 @@ points = np.array([
 x = points[:, 0]
 y = points[:, 1]
 
-slope, intercept, r, p, std_err = stats.linregress(x, y)
+correlation_coefficient = pd.DataFrame.corr(pd.DataFrame(points)).loc[1][0]
+stdX= np.std(x)
+stdY= np.std(y)
+
+slope = (stdY/stdX) * correlation_coefficient
+
+intercept = np.mean(y) - slope * np.mean(x)
 
 def line(x):
     return slope * x + intercept
